a4c0650bf465885710d057cf4f3bfb8100707bf4
Salashnyi Ivan <salashniy12@gmail.com>
2018-12-24 19:22:01 +0200

    adding/remoting departments, adding workers

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.get('*', function(req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+app.listen(8080);
