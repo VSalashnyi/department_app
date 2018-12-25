ae24daa73249e8637c75d89eaab9a3185582182c
SalashniyIvan <salashniy12@gmail.com>
2018-12-25 23:10:59 +0200

    editing users

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 
+
 app.use(morgan('dev'));
+
 
 app.get('*', function(req, res) {
     res.sendfile(__dirname + '/public/index.html');
